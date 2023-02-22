d48df74920e1d9d3905a6d5c1c33b4da62c33e9f
Khuks <khulekanimkholo30@gmail.com>
2023-02-22 22:19:52 +0200

    Updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ st.set_page_config(layout='wide',initial_sidebar_state='expanded')
 
 # Hide hurmburger Menu
 
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style',unsafe_allow_html=True)
