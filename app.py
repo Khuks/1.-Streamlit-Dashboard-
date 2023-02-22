59bd4e6c104d8101b26aaa0e349450c114b65dec
Khuks <khulekanimkholo30@gmail.com>
2023-02-23 01:56:35 +0200

    Updated Harmburger Menu

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import plost
 st.set_page_config(layout='wide',initial_sidebar_state='expanded')
 
 # Hide hurmburger Menu
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style',unsafe_allow_html=True)
